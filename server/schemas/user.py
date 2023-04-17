94bbfeb26548712b1876acf4873412fc279207d9
Zsigmond Péter <zspeti02@gmail.com>
2023-04-17 21:37:19 +0200

    Add username and password constraints

--- server/schemas/user.py
+++ server/schemas/user.py
@@ -1,12 +1,12 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class UserBase(BaseModel):
-    username: str
+    username: str = Field(min_length = 3, max_length = 100)
 
 
 class UserCreate(UserBase):
-    password: str
+    password: str = Field(min_length = 8, max_length = 100)
 
 
 class User(UserBase):
